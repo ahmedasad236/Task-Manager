09cc96eb26613cef852b1fb3a9e4bcb120d76f5e
ahmedasad236 <ahmedasad236@gmail.com>
2024-11-08 22:38:10 +0200

    Fix the local storage issue
    - It was clearing the tasks on every refresh
    - The problem was because of the client comoponent is also rendered at
      the server for the first render and it could not access the
    localstorage, so it sets the task with []
    
    - The solution: check for that the code is running on the client by
      checking the 'window' object and then initialize the tasks

--- app/_contexts/TaskContext.tsx
+++ app/_contexts/TaskContext.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import Toast from '../_components/Toast';
 
@@ -22,19 +23,27 @@ export const TaskProvider: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [isInitialized, setIsInitialized] = useState(false);
   const [toast, setToast] = useState<{
     message: string;
     error?: boolean;
   } | null>(null);
 
   useEffect(() => {
-    const storedTasks = localStorage.getItem('tasks');
-    if (storedTasks) setTasks(JSON.parse(storedTasks));
+    // Only access localStorage on the client side, because it was not defined on the server side and results in empty tasks
+    if (typeof window !== 'undefined') {
+      const storedTasks = localStorage.getItem('tasks');
+      if (storedTasks) setTasks(JSON.parse(storedTasks));
+      setIsInitialized(true); // Mark initialization as complete
+    }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
+    // Ensure localStorage is only updated once tasks are loaded
+    if (isInitialized) {
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }, [tasks, isInitialized]);
 
   const showToast = (message: string, error: boolean) => {
     setToast({ message, error });
@@ -42,37 +51,31 @@ export const TaskProvider: React.FC<{ children: React.ReactNode }> = ({
   };
 
   const addTask = (task: Task) => {
-    // check if there is a task with the same title
     const taskExists = tasks.find((t) => t.title === task.title);
     if (taskExists) {
       showToast('Task with the same title already exists!', true);
       return;
     }
-
     setTasks([...tasks, task]);
     showToast('Task created successfully!', false);
   };
 
   const updateTask = (id: string, updatedTask: Task) => {
-    // check if there is a task with the same title
     const task = tasks.find((t) => t.id === id);
     if (!task) {
       showToast('Task not found!', true);
       return;
     }
-
     setTasks(tasks.map((task) => (task.id === id ? updatedTask : task)));
     showToast('Task updated successfully!', false);
   };
 
   const deleteTask = (id: string) => {
-    // check if there is a task with the same title
     const task = tasks.find((t) => t.id === id);
     if (!task) {
       showToast('Task not found!', true);
       return;
     }
-
     setTasks(tasks.filter((task) => task.id !== id));
     showToast('Task deleted successfully!', false);
   };
